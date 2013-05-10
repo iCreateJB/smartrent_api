87542ce60959a000d440e6cfbaf8f54e6e6ca97c
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-05-10 19:59:26 -0400

    Adding scopes for Renter.

--- app/models/renter.rb
+++ app/models/renter.rb
@@ -15,4 +15,26 @@ class Renter
   one  :current_application, :class_name => "RentalApplication"
   many :sent_rental_applications, :class_name => "SentRentalApplication"
 
+  # scopes
+  def self.unconfirmed_accounts
+    find_with_conditions(:confirmed_at => nil)
+  end
+
+  def self.enrolled_accounts
+    list.delete_if{ |i| i.confirmed_at.nil? }
+  end
+
+  def self.enrolled_by_date(options={})
+    case 
+    when options.has_key?(:start_date) && options.has_key?(:end_date)
+      enrolled_accounts.keep_if{ |u| u.confirmed_at >= options[:start_date].to_datetime && u.confirmed_at <= options[:end_date].to_datetime }
+    when options.has_key?(:start_date) && !options.has_key?(:end_date)
+      enrolled_accounts.keep_if{ |u| u.confirmed_at >= options[:start_date].to_datetime }
+    when !options.has_key?(:start_date) && options.has_key?(:end_date)      
+      enrolled_accounts.keep_if{ |u| u.confirmed_at <= options[:end_date].to_datetime }
+    else
+      enrolled_accounts
+    end
+  end
+
 end
