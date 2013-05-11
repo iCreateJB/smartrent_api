28d4cb51d80e83afb376166bd1661a463b71dca7
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-05-10 21:46:18 -0400

    Return Enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,6 +2,7 @@ class EnrollmentsController < ApplicationController
   respond_to :json
   
   def show
-    
+    @enrollments = EnrollmentServices.by_date(params)
+    respond_with EnrollmentServicesPresenter.response(@enrollments)
   end
 end
