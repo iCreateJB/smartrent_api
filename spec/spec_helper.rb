cc8d702421a6d917fac008de170f8011c16b97d2
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-05-10 21:44:50 -0400

    Load Factories.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'factory_girl'
+
+FactoryGirl.definition_file_paths = %w(#{config.root}/spec/factories)
+FactoryGirl.find_definitions
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
