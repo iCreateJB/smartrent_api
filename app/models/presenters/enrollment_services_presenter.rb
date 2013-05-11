5d2d95a79348ee41416f268eed9027bb118c8169
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-05-10 21:27:06 -0400

    EnrollmentServicesPresenter

---
+++ app/models/presenters/enrollment_services_presenter.rb
@@ -0,0 +1,30 @@
+class EnrollmentServicesPresenter
+  
+  attr_accessor :options, :count
+
+  def self.response
+    self.new(options).response
+  end
+
+  def initialize(options={})
+    @options = options
+    @count   = 0
+  end
+
+  def response
+    {
+      :count       => @options.size, 
+      :enrollments => [
+        @options.each do |i|
+          {
+            :email      => i.email, 
+            :first_name => i.first_name,
+            :last_name  => i.last_name,
+            :renting_in_state_code    => i.renting_in_state_code,
+            :sent_rental_applications => i.sent_applications.size
+          }
+        end
+      ]
+    }
+  end
+end
