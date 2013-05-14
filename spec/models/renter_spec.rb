08b6eb36f8e0825b8e86aba16470f8bf674e67ab
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-05-13 20:45:39 -0400

    Update methods.

--- spec/models/renter_spec.rb
+++ spec/models/renter_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Renter do 
+  let(:renter){ FactoryGirl.create(:renter, :confirmed_at => Date.today) }
+
   it { Renter.properties.include?(:first_name).should be_true }
   it { Renter.properties.include?(:last_name).should be_true }
   it { Renter.properties.include?(:email).should be_true }
@@ -15,7 +17,26 @@ describe Renter do
 
   subject{ Renter.new }
 
+  before(:each) do 
+    renter
+  end
+
   it { subject.class.should respond_to(:unconfirmed_accounts) }
   it { subject.class.should respond_to(:enrolled_accounts) }
   it { subject.class.should respond_to(:enrolled_by_date) }
+
+  it "should return all enrolled accounts" do 
+    FactoryGirl.create(:renter, :confirmed_at => nil)
+    subject.class.enrolled_accounts.size.should == 1
+  end
+
+  it "should return all confirmed enrollments_by_date" do 
+    subject.class.enrolled_by_date.include?(renter).should be_true
+  end
+
+  it "should return all unconfirmed enrollments" do 
+    renter.confirmed_at = nil
+    renter.save
+    subject.class.unconfirmed_accounts.include?(renter).should be_true
+  end
 end
