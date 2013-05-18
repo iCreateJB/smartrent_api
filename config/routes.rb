9ca5ae6d8c869337e5da5229c659d811ab431a9a
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-05-18 18:59:21 -0400

    Update landlord resource as well as add status route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 SmApi::Application.routes.draw do
   scope '/v1', :via => :get do 
     match 'renter_enrollments'   => "enrollments#show",      :type => "renter"
-    match 'landlord_enrollments'=> "enrollments#show",      :type => "landlord"
-    match 'applications'  => "landlords#applications"
-    match 'renter'        => "renters#show"
+    match 'landlord_enrollments' => "enrollments#show",      :type => "landlord"
+    match 'applications'         => "landlords#show"
+    match 'renter'               => "renters#show"
+    match 'status'               => "index#show"
   end
 end
