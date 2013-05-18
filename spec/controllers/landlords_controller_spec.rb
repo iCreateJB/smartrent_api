eef06009eac3de15b9c7310bdb0cbbcc5fd13fe8
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-05-18 19:04:35 -0400

    Update LandlordsController Spec

--- spec/controllers/landlords_controller_spec.rb
+++ spec/controllers/landlords_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe LandlordsController do
+  let(:landlord){ FactoryGirl.create(:landlord, :confirmed_at => Date.today )}
+
   it { should respond_to(:show)}
+
+  before(:each) do 
+    landlord
+  end
+
+  it "should return landlord information" do 
+    response = get :show, :landlord => landlord.email, :format => :json
+    JSON.parse(response.body)['email'].should == landlord.email
+  end
 end
