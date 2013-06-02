58f2127dcd3c7b22fd37f247bcc22dd9a44b67b2
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-06-02 15:27:33 -0400

    Update references.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,10 +4,10 @@ set :user, 'root'
 set :domain, "ec2-23-23-34-20.compute-1.amazonaws.com"
 ssh_options[:keys] = ["#{ENV['HOME']}/staging.pem"]
 set :application, 'smartrent'      # eg 'rfx'
-set :deploy_to, '/var/www/smartrent.pro'
+set :deploy_to, '/var/www/smartrent_api'
 set :scm_passphrase, 'sinister'
 
 set :keep_releases, 10
-set :branch, 'design-refresh'
+set :branch, 'master'
 set :rails_env, "staging"
 set :use_sudo, false
