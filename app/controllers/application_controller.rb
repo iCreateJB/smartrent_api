387339d14a58e234df0a66d874a1a7388018336e
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-06-11 21:54:23 -0400

    Authentication is now done through an HMAC Authorization token of the signed request params.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,26 @@ class ApplicationController < ActionController::Base
   before_filter :restrict_access
 
   def restrict_access
-    authenticate_or_request_with_http_token do |token, options|
-      !ApiKey.find_with_conditions(access_token: token).empty?
-    end
+    begin 
+      begin
+        secret = ApiKey.find(params[:api_key].to_s).secret
+      rescue
+        raise StandardError.new("Could not find user with key: #{param[:api_key]}")
+      end
+
+      authentication_hash = URI.decode(request.headers["AUTHORIZATION"])
+      raise StandardError.new("No HASH provided") if authentication_hash.blank?
+
+      computed_token = Base64.strict_encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha256'), secret, URI.decode(request.query_string)))
+      unless authentication_hash == computed_token
+        raise StandardError.new("Token mismatch when checking authentication")
+      end
+    rescue StandardError
+      not_authorized
+    end 
+  end
+
+  def not_authorized
+    render :json => {}, :status => :unauthorized
   end
 end
