705c6537df805bf4c89bc99db07f948083990f15
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-06-16 08:15:40 -0400

    Update version. Moving from v1 to v2.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SmApi::Application.routes.draw do
-  scope '/v1', :via => :get do 
+  scope '/v2', :via => :get do 
     match 'renter_enrollments'   => "enrollments#show",      :type => "renter"
     match 'landlord_enrollments' => "enrollments#show",      :type => "landlord"
     match 'landlords'            => "landlords#show"
--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 
 describe "Routes" do 
-  it "GET => '/v1/renter_enrollments'" do 
-    expect(:get => '/v1/renter_enrollments').to route_to(:controller => 'enrollments', :action => 'show', :type => "renter")
+  it "GET => '/v2/renter_enrollments'" do 
+    expect(:get => '/v2/renter_enrollments').to route_to(:controller => 'enrollments', :action => 'show', :type => "renter")
   end
 
-  it "GET => '/v1/landlord_enrollments'" do 
-    expect(:get => '/v1/landlord_enrollments').to route_to(:controller => 'enrollments', :action => 'show', :type => "landlord")
+  it "GET => '/v2/landlord_enrollments'" do 
+    expect(:get => '/v2/landlord_enrollments').to route_to(:controller => 'enrollments', :action => 'show', :type => "landlord")
   end
 
-  it "GET => '/v1/landlords'" do 
-    expect(:get => '/v1/landlords').to route_to(:controller => 'landlords', :action => 'show')
+  it "GET => '/v2/landlords'" do 
+    expect(:get => '/v2/landlords').to route_to(:controller => 'landlords', :action => 'show')
   end
 
-  it "GET => '/v1/renters'" do 
-    expect(:get => '/v1/renters').to route_to(:controller => 'renters', :action => 'show')
+  it "GET => '/v2/renters'" do 
+    expect(:get => '/v2/renters').to route_to(:controller => 'renters', :action => 'show')
   end
 
-  it "GET => '/v1/status'" do 
-    expect(:get => '/v1/status').to route_to(:controller => 'index', :action => 'show')
+  it "GET => '/v2/status'" do 
+    expect(:get => '/v2/status').to route_to(:controller => 'index', :action => 'show')
   end
 end
